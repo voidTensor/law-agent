513ab2827ee3f4b28ef1de057867a79a27d507aa
Binjie Zhang <102460641+voidTensor@users.noreply.github.com>
2025-07-09 19:15:16 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args)); // 兼容node-fetch
 const cheerio = require('cheerio'); // 用于解析HTML
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 设置中间件
 app.use(cors());
